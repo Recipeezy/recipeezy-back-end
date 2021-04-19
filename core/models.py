7dc50b1131fe219cc9f62518d6524548d0c0a02e
GrantDM <mullen.d.grant@gmail.com>
2021-04-19 16:28:14 -0400

    models made

--- core/models.py
+++ core/models.py
@@ -1,5 +1,51 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class User(AbstractUser):
     pass
+
+
+class Pantry(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    pantry = models.ForeignKey(Pantry, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    category = models.CharField(max_length=100, null=True, blank=True)
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through = 'RecipeIngredient',
+        through_fields = ('recipe', 'ingredient')
+    )
+    pantry = models.ForeignKey(Pantry, on_delete=models.CASCADE)
+    origin = models.CharField(max_length=100, null=True, blank=True)
+    instructions = models.TextField(max_length=2500, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    measurement = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.recipe}, {self.ingredient}'
+
+
+
--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 from rest_framework import generics
+from .models import User, Pantry, Recipe, RecipeIngredient, Ingredient
 
 # Create your views here.
